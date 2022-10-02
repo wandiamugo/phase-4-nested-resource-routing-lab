68b11ce20e2caddc8d3220618ff53f8bd5394624
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-10-02 14:09:01 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
-    render json: items, include: :user
+    end
+    return render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+
+    return render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+
 
 end
